b97b244e76c7d9409dfa28de5e5e73e7ae248c36
Andrey Fedoseev <andrey@andreyfedoseev.com>
2023-01-27 22:04:59 +0600

    enable wildcards `*` in patterns

--- src/pattern.rs
+++ src/pattern.rs
@@ -4,6 +4,21 @@ pub trait Pattern {
 
 impl Pattern for String {
     fn matches(&self, url: &str) -> bool {
-        url.contains(self)
+        if self.contains('*') {
+            let pat = wildflower::Pattern::new(self);
+            pat.matches(url)
+        } else {
+            url.contains(self)
+        }
     }
+}
+
+#[test]
+fn test_matching() {
+    assert!("example.com"
+        .to_string()
+        .matches("https://www.example.com/"));
+    assert!("https://*.example.com/*"
+        .to_string()
+        .matches("https://www.example.com/"));
 }
