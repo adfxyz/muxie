41602548f768ddc2da3822721e78af3caf487b3e
Andrey Fedoseev <andrey@andreyfedoseev.com>
2023-01-28 15:13:19 +0600

    add docs and more tests to `Pattern.matches`

--- src/pattern.rs
+++ src/pattern.rs
@@ -3,6 +3,14 @@ pub trait Pattern {
 }
 
 impl Pattern for String {
+    /// Check if a URL matches a pattern.
+    /// Pattern may contain `*` wildcard that matches any number of characters.
+    /// If there is no `*` in the pattern, it is treated as a substring.
+    ///
+    /// Examples:
+    /// - example.com matches https://www.example.com/any/path
+    /// - example.com also matches https://domain.com?redirect=https://www.example.com
+    /// - https://*.example.com/*/index.html matches https://www.example.com/any/path/index.html
     fn matches(&self, url: &str) -> bool {
         if self.contains('*') {
             let s = self.replace('?', r"\?");
@@ -19,9 +27,15 @@ fn test_matching() {
     assert!("example.com"
         .to_string()
         .matches("https://www.example.com/"));
+    assert!("example.com"
+        .to_string()
+        .matches("https://domain.com?redirect=https://www.example.com"));
     assert!("https://*.example.com/*"
         .to_string()
         .matches("https://www.example.com/"));
+    assert!(!"https://*.example.com/*"
+        .to_string()
+        .matches("https://domain.com?redirect=https://www.example.com"));
     assert!(!"*.example.com/path?foo"
         .to_string()
         .matches("https://www.example.com/path/foo"));
