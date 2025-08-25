66b71272ad8575cf8b42e27e75af1c7666165a7a
Andrey Fedoseev <andrey@andreyfedoseev.com>
2025-08-25 10:48:30 +0500

    Update cargoHash

--- default.nix
+++ default.nix
@@ -2,6 +2,6 @@
 rustPlatform.buildRustPackage {
   pname = "muxie";
   version = "0.1.1";
-  cargoHash = "sha256-CnC+8HnjEmsGYuPVrk/m+maTeFg3qFzMbxZLSXRqzyw=";
+  cargoHash = "sha256-gvJxG1IRKCjeZUDoPHKeL8s05KSaArrDz9AzWvBdlwA=";
   src = pkgs.lib.cleanSource ./.;
 }
